37e618f5160a23da81a408e9763cf42e6ef179f2
cchanpanhavuth <chanpanhavuth.chea@gmail.com>
2024-05-28 15:50:24 +0700

    udpate server to have route

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
+const userRoutes = require("./routers/userRoutes")
+const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config();
-
-
-const app = express();
 const port = process.env.PORT;
 
-app.use("/assets", express.static("public"));
-
+app.use(express.json())
+app.use("/assets", express.static("public"))
+app.use("/v1",userRoutes)
+app.get("/", (req, res) => {
+  const style = "stylesheet"
+  res.send("<link rel='"+ style +"' href='/assets/css/styles.css'><h1 style='color: red'><img src='/assets/img/landscape.webp'>NodeJS Project</h1>")
+})
 
 app.listen(port, () =>{
     console.log(`Server started on port http://localhost:${port}`);
