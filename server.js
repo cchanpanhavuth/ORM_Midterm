32db304a3eeecd7632f26e77b7d9a29f3a4c2497
Neathhhh <129174273+Neathhhh@users.noreply.github.com>
2024-06-04 18:56:28 +0700

    set path in server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const userRoutes = require("./src/routes/userRoutes")
 const roleRoutes = require("./src/routes/roleRoutes")
 const app = express();
 const dotenv = require('dotenv');
+const categoryRoutes = require("./src/routes/categoryRoute");
 
 dotenv.config();
 const port = process.env.PORT;
@@ -10,6 +11,7 @@ const port = process.env.PORT;
 app.use(express.json())
 app.use("/assets", express.static("public"))
 app.use("/v1",userRoutes, roleRoutes)
+app.use('/v1',categoryRoutes)
 app.get("/", (req, res) => {
   const style = "stylesheet"
   res.send("<link rel='"+ style +"' href='/assets/css/styles.css'><h1 style='color: red'><img src='/assets/img/landscape.webp'>NodeJS Project</h1>")
