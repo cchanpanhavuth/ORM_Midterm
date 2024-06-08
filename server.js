9e6427485e08c0a9a2b1647066e030ecfed9c98f
cchanpanhavuth <chanpanhavuth.chea@gmail.com>
2024-06-08 21:14:07 +0700

    add additional routes
    added routes for role

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require("express");
 const commentRoute = require("./src/routes/commentRoute")
 const contentRoute = require("./src/routes/contentRoute")
 const userRoutes = require("./src/routes/userRoutes")
+const roleRoutes = require("./src/routes/roleRoutes")
+const categoryRoutes = require("./src/routes/categoryRoute");
 const app = express();
 const dotenv = require('dotenv');
-const categoryRoutes = require("./src/routes/categoryRoute");
 
 dotenv.config();
 const port = process.env.PORT;
 
 app.use(express.json())
 app.use("/assets", express.static("public"))
-app.use("/v1",userRoutes, roleRoutes,categoryRoutes)
+app.use("/v1",userRoutes, roleRoutes,categoryRoutes, contentRoute, commentRoute)
 app.get("/", (req, res) => {
   const style = "stylesheet"
   res.send("Welcome to Server.")
